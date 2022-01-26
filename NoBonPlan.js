76189996b3ebbdb97ebb606f837bc71d125dde55
GuiguiRoxxor34 <maurin.gui@gmail.com>
2022-01-26 17:22:26 +0100

    gestion grands et petits livefeeds en page d'acceuil

--- NoBonPlan.js
+++ NoBonPlan.js
@@ -13,8 +13,10 @@
     'use strict';
    //BON PLANS page d'acceuil JVC
     [...document.getElementsByClassName("card__contentType")].filter(x=>x.innerText=='NEWS BON PLAN'&&x.classList.length==4).forEach(y=>y.parentNode.parentNode.parentNode.parentNode.parentNode.remove());
-	//Live feeds acceuil jvc
-	 [...document.getElementsByClassName("card__contentType")].filter(x=>x.innerText=='LIVE FEED').forEach(y=>y.parentNode.parentNode.parentNode.parentNode.remove());
+	//Live feeds acceuil jvc (petits)
+	 [...document.getElementsByClassName("card__contentType")].filter(x=>x.innerText=='LIVE FEED'&&x.parentNode.parentNode.parentNode.parentNode.clientHeight<300).forEach(y=>y.parentNode.parentNode.parentNode.parentNode.remove());
+	 // (grands)
+	 [...document.getElementsByClassName("card__contentType")].filter(x=>x.innerText=='LIVE FEED'&&x.parentNode.parentNode.parentNode.parentNode.clientHeight>300).forEach(y=>y.parentNode.parentNode.parentNode.remove());
     // news mise en avant
      [...document.getElementsByClassName("badge bg-primary text-uppercase text-truncate mw-100 card__contentType")].filter(x=>x.innerText=='NEWS BON PLAN').forEach(y=> {y.parentNode.parentNode.parentNode.parentNode.style.marginTop = '-265px';y.parentNode.parentNode.parentNode.remove();});
    //page "toutes les news"
