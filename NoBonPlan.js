63f9fdc4d4db4c1c3ea46e1dbd8211108be9d379
GuiguiRoxxor34 <maurin.gui@gmail.com>
2022-01-28 10:28:04 +0100

    correction decalage  grandes news bonplan

--- NoBonPlan.js
+++ NoBonPlan.js
@@ -18,7 +18,7 @@
 	 // (grands)
 	 [...document.getElementsByClassName("card__contentType")].filter(x=>x.innerText=='LIVE FEED'&&x.parentNode.parentNode.parentNode.parentNode.clientHeight>300).forEach(y=>y.parentNode.parentNode.parentNode.remove());
     // news mise en avant
-     [...document.getElementsByClassName("badge bg-primary text-uppercase text-truncate mw-100 card__contentType")].filter(x=>x.innerText=='NEWS BON PLAN').forEach(y=> {y.parentNode.parentNode.parentNode.parentNode.style.marginTop = '-265px';y.parentNode.parentNode.parentNode.remove();});
+     [...document.getElementsByClassName("badge bg-primary text-uppercase text-truncate mw-100 card__contentType")].filter(x=>x.innerText=='NEWS BON PLAN').forEach(y=> {if (y.parentNode.parentNode.parentNode.clientHeight<400){y.parentNode.parentNode.parentNode.parentNode.style.marginTop = '-265px';}y.parentNode.parentNode.parentNode.remove();});
    //page "toutes les news"
     setTimeout(function(){
     [...document.getElementsByClassName("typeTitle__2ceWaU")].filter(x=>x.innerText=='News bon plan').forEach(y=>y.parentNode.parentNode.parentNode.parentNode.remove());
