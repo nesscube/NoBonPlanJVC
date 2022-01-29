72ae32d82c273ac082ce07dab0c6a8592d6d6350
GuiguiRoxxor34 <maurin.gui@gmail.com>
2022-01-29 02:20:53 +0100

    ajout timeout pour eviter les pertes d'elements

--- NoBonPlan.js
+++ NoBonPlan.js
@@ -11,16 +11,16 @@
 
 (function() {
     'use strict';
-   //BON PLANS page d'acceuil JVC
-    [...document.getElementsByClassName("card__contentType")].filter(x=>x.innerText=='NEWS BON PLAN'&&x.classList.length==4).forEach(y=>y.parentNode.parentNode.parentNode.parentNode.parentNode.remove());
-	//Live feeds acceuil jvc (petits)
-	 [...document.getElementsByClassName("card__contentType")].filter(x=>x.innerText=='LIVE FEED'&&x.parentNode.parentNode.parentNode.parentNode.clientHeight<300).forEach(y=>y.parentNode.parentNode.parentNode.parentNode.remove());
-	 // (grands)
-	 [...document.getElementsByClassName("card__contentType")].filter(x=>x.innerText=='LIVE FEED'&&x.parentNode.parentNode.parentNode.parentNode.clientHeight>300).forEach(y=>y.parentNode.parentNode.parentNode.remove());
-    // news mise en avant
-     [...document.getElementsByClassName("badge bg-primary text-uppercase text-truncate mw-100 card__contentType")].filter(x=>x.innerText=='NEWS BON PLAN').forEach(y=> {if (y.parentNode.parentNode.parentNode.clientHeight<400){y.parentNode.parentNode.parentNode.parentNode.style.marginTop = '-265px';}y.parentNode.parentNode.parentNode.remove();});
-   //page "toutes les news"
-    setTimeout(function(){
-    [...document.getElementsByClassName("typeTitle__2ceWaU")].filter(x=>x.innerText=='News bon plan').forEach(y=>y.parentNode.parentNode.parentNode.parentNode.remove());
-    },500);
+        setTimeout(function(){
+            //BON PLANS page d'acceuil JVC
+            [...document.getElementsByClassName("card__contentType")].filter(x=>x.innerText=='NEWS BON PLAN'&&x.classList.length==4).forEach(y=>y.parentNode.parentNode.parentNode.parentNode.parentNode.remove());
+            //Live feeds acceuil jvc (petits)
+            [...document.getElementsByClassName("card__contentType")].filter(x=>x.innerText=='LIVE FEED'&&x.parentNode.parentNode.parentNode.parentNode.clientHeight<300).forEach(y=>y.parentNode.parentNode.parentNode.parentNode.remove());
+            // (grands)
+            [...document.getElementsByClassName("card__contentType")].filter(x=>x.innerText=='LIVE FEED'&&x.parentNode.parentNode.parentNode.parentNode.clientHeight>300).forEach(y=>y.parentNode.parentNode.parentNode.remove());
+            // news mise en avant
+            [...document.getElementsByClassName("badge bg-primary text-uppercase text-truncate mw-100 card__contentType")].filter(x=>x.innerText=='NEWS BON PLAN').forEach(y=> {if (y.parentNode.parentNode.parentNode.clientHeight<400){y.parentNode.parentNode.parentNode.parentNode.style.marginTop = '-265px';}y.parentNode.parentNode.parentNode.remove();});
+            //page "toutes les news"
+            [...document.getElementsByClassName("typeTitle__2ceWaU")].filter(x=>x.innerText=='News bon plan').forEach(y=>y.parentNode.parentNode.parentNode.parentNode.remove());
+    },100);
 })();
